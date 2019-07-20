08489e9c04df343ea9417a878086cd4aa7089fc9
firdausa7 <firdausa.salat@gmail.com>
2019-07-20 14:29:55 +0300

    add jquery edits

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("div#d1").click(function () {
+        $("#d1").hide();
+        $("#p1").show();
+    });
+});
+$("#p1").click(function () {
+    $("#p1").hide();
+    $("#d1").show();
+});
+$("div#d2").click(function () {
+    $("#d2").hide();
+    $("#p2").show();
+});
+$("#p2").click(function () {
+    $("#p2").hide();
+    $("#d2").show();
+});
+$("div#d3").click(function () {
+    $("#d3").hide();
+    $("#p3").show();
+});
+$("#p3").click(function () {
+    $("#p3").hide();
+    $("#d3").show();
 });
